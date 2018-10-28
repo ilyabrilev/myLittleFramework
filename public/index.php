7b2b8ef60056c4e24f63bc61b57f0a62e74b7ba0
ilyabrilev <ilyabrilev@yandex.ru>
2018-10-28 21:26:32 +0800

    cleaning

--- public/index.php
+++ public/index.php
@@ -3,16 +3,6 @@
 require '../vendor/autoload.php';
 require '../HelperFunctions.php';
 
-/*
-require '../core/HelperFunctions.php';
-require '../core/Router.php';
-require '../core/Request.php';
-require '../core/Configuration.php';
-require '../core/DBConnection.php';
-require '../core/Environment.php';
-require '../app/Models/User.php';
-*/
-
 $router = new \Core\Router();
 $request = \Core\Request::Get();
 $errorHandler = new \Core\ErrorHandler();
@@ -24,12 +14,3 @@ catch (\Exception $ex) {
     $errorHandler->Handle($ex, $request);
 }
 
-
-//return $result;
-
-//echo \Utilities\Environment::Get('CONNECTION_HOST_TYPE');
-
-//jdd(\Models\User::getAll());
-//jdd(\Utilities\DBConnection::GetInstance()->RawFetch('SELECT * FROM users'));
-
-//var_dump(\Utilities\Configuration::getDB());
